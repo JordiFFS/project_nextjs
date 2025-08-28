e05e1e2d9c877e997f83de7bf5f0f6f863dfceac
JordiFFS <117864372+JordiFFS@users.noreply.github.com>
2025-08-28 13:11:33 -0500

    [UPADTE]

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar: React.FC = () => {
+  return (
+    <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>NavBar</div>
+  );
+};
+
+export default NavBar;
